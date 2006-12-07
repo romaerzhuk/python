59446c60fdac3c11486b2983cc0781aeb579bc6e
Roman <Roman@b4698315-7dff-e942-a971-4591eb902e80>
2006-12-07 15:54:37 +0000

    переход на Python2.5
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@149 b4698315-7dff-e942-a971-4591eb902e80

--- LogFiler.py
+++ LogFiler.py
@@ -1,21 +1,25 @@
-import os, dospath, re, FileFilter, ListFile
+# -*- coding: cp1251 -*-
+
+import os, re, FileFilter, ListFile
 
 if __name__=='__main__':
   path='log'
-  logLevel='WARN'
-  logCode='24'
+  logLevel='INFO'
+  logCode='0'
   strSearch='\d+'
-  reStr='^(\d\d\.\d\d\.\d\d\d\d \d\d:\d\d:\d\d\.\d\d\d): '+logLevel+'[ ]*:[ ]*'+logCode+':.*-(\d*)$'
+  reStr='^(\d\d\.\d\d\.\d\d\d\d \d\d:\d\d:\d\d)\.\d\d\d: ' \
+  	+logLevel+'[ ]*:[ ]*'+logCode \
+  	+':.*длина=(\d+\.?\d*) м.*\{1, ([+-]?\d*\.?\d+) см\}, \{1, ([+-]?\d*\.?\d+) см\}\]$'
   reVal=re.compile(reStr)
   logFile=FileFilter.Filter(['*.log'])
   logList=[]
   for file in os.listdir(path):
-    if not dospath.isdir(file) and logFile(file):
+    if not os.path.isdir(file) and logFile(file):
       logList.append(path+'/'+file)
   logList.sort()
   for file in logList:
       for line in ListFile.List(file):
         m=reVal.search(line)
         if m != None:
-          print m.group(1), m.group(2)
+          print m.group(1), m.group(2), m.group(3), m.group(4)
 
