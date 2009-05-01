28677d97f474420cc0a7fd3c3b263f33cdb1a82e
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2009-05-01 09:46:00 +0000

    import
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@662 b4698315-7dff-e942-a971-4591eb902e80

---
+++ bzr-remove-deadheads.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import sys, os
+
+class RemoveDeadHeads:
+  def __init__(self, options, tmp):
+    self.tmp = tmp
+    self.system("bzr init-repo %1s %2s" % (options, tmp))
+    self.clone('.')
+    os.rename(".bzr", tmp + '/' + tmp)
+    os.rename(tmp + "/.bzr", ".bzr")
+    self.removedir(tmp)
+  def clone(self, path):
+    if ".bzr" == path or self.tmp == path:
+      return
+    if ".bzr" == os.path.basename(path):
+      print "clone(%1s)" % path
+      path = os.path.dirname(path)
+      mkdirs(self.tmp + '/' + os.path.dirname(path))
+      self.system("bzr branch %1s %2s" % (path, self.tmp + '/' + path))
+      return
+    if path == '.': prefix = ''
+    else: prefix = path + '/'
+    for i in os.listdir(path):
+      self.clone(prefix + i)
+  def system(self, command):
+    res = os.system(command)
+    if res != 0:
+      raise IOError("command '%1s': res=%2s" % (command, res))
+  def removedir(self, path):
+    for i in os.listdir(path):
+      s = path + '/' + i
+      if os.path.isfile(s):
+        os.remove(s)
+      else:
+        self.removedir(s)
+    os.rmdir(path)
+
+# Создаёт вложенные директории.
+# Если директории уже существуют, ничего не делает
+def mkdirs(path):
+  #print "mkdirs(" + path + ")"
+  if os.path.exists(path):
+    return
+  mkdirs(os.path.dirname(path))
+  os.mkdir(path)
+
+if __name__ == "__main__":
+  RemoveDeadHeads("--1.9-rich-root --no-tree", "bzr-repo.tmp")
