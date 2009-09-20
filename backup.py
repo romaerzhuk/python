0dc35491abb575c71bffc24ec7675d0b87ac4ab6
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2009-09-20 16:00:52 +0000

    .md5 хранит контрольные суммы всех файлов в директории
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@764 b4698315-7dff-e942-a971-4591eb902e80

--- backup.py
+++ backup.py
@@ -4,7 +4,7 @@
 import sys, os, re, time, hashlib, socket, tarfile, shutil, platform, time
 
 # Рекурсивно сканирует директорию. Вызывает для каждой директории процедуру
-def through_dirs(path, proc=None, fileFilter=None):
+def through_dirs(path, proc = None, fileFilter = None):
   if path == '.': prefix = ''
   else: prefix = path + '/'
   if fileFilter == None or fileFilter(path):
@@ -23,6 +23,11 @@ def md5sumCreate(file):
   try:
     fd.write("%s\t*%s\n" % (md5sum(file).hexdigest(), os.path.basename(file)))
   finally:
+    close(fd)
+
+# Закрывает файл с проверкой на None
+def close(fd):
+  if fd != None:
     fd.close()
 
 # Засекает время выполнения команды
@@ -31,21 +36,21 @@ class StopWatch:
     self.msg = msg
     self.start = time.time()
   def stop(self):
-    print "[%1s]: %2s sec" % (self.msg, time.time()-self.start)
+    print "[%1s]: %2s sec" % (self.msg, time.time() - self.start)
 
-# Вычисляет контрольную сумму файла
+# Вычисляет контрольную сумму файла в шестнадцатиричном виде
 def md5sum(file):
-  sw = StopWatch("md5sum(%1s)" % file)
+  sw = StopWatch("md5sum %1s" % file)
   fd = open(file, "rb")
   try:
     sum = hashlib.md5()
     while True:
       buf = fd.read(1024 * 1024)
       if len(buf) == 0:
-        return sum
+        return sum.hexdigest().lower()
       sum.update(buf)
   finally:
-    fd.close() 
+    close(fd)
     sw.stop()
 
 # Создаёт вложенные директории.
@@ -63,7 +68,7 @@ def readline(file):
   try:
     return fd.readline()
   finally:
-    fd.close()
+    close(fd)
 
 # Вызывает системную команду и выводит эхо на стандартный вывод
 def system(command):
@@ -76,14 +81,14 @@ def system(command):
 def chdir(dir):
   print "cd", dir
   os.chdir(dir)
-  
+
 # Проверяет корректность файлов svn-репозиториев
 def svnVerify(dir):
-  svnList = ((1,"conf"), (1,"db"), (1,"hooks"), (1,"locks"), (0,"README.txt"))
+  svnList = ((1, "conf"), (1, "db"), (1, "hooks"), (1, "locks"), (0, "README.txt"))
   dirSet = set(os.listdir(dir))
-  for type,name in svnList:
+  for type, name in svnList:
     path = dir + '/' + name
-    if name not in dirSet or type==1 and not os.path.isdir(path) or type==0 and not os.path.isfile(path):
+    if name not in dirSet or type == 1 and not os.path.isdir(path) or type == 0 and not os.path.isfile(path):
       return True
   if not readline(dir + "/README.txt").startswith("This is a Subversion repository;"):
     return True
@@ -105,7 +110,7 @@ def bzrVerify(dir):
     try:
       reParent = re.compile(r"^parent_location\s*=")
       reBound = re.compile(r"^bound\s*=\s*False")
-      parent = False 
+      parent = False
       bound = True
       for line in f:
         if reParent.match(line): parent = True
@@ -117,7 +122,7 @@ def bzrVerify(dir):
     elif parent:
       chdir(bzr)
       system("bzr pull")
-  if os.path.isdir(dir + "/repository"): 
+  if os.path.isdir(dir + "/repository"):
     notWin = platform.system() != "Windows"
     res = system("bzr check %1s" % bzr)
     if res != 0 and notWin:
@@ -156,14 +161,15 @@ def repoVerify(dir):
 
 # Файл, подготовленный к восстановлению
 class RecoveryEntry:
-  def __init__(self,name):
-    self.dir  = None # имя директории с корректным файлом
-    self.name = name # имя файла
-    self.list = []   # список директорий, куда нужно восстанавливать файл
-  def __cmp__(self,entry):
+  def __init__(self, name):
+    self.name = name  # имя файла
+    self.md5 = dict() # контрольная сумма файла в соответствущей директории
+    self.dir = None   # имя директории с корректным файлом
+    self.list = []    # список директорий, куда нужно восстанавливать файл
+  def __cmp__(self, entry):
     if self.dir != None:
       if entry.dir == None:
-        return -1
+        return - 1
     elif entry.dir != None:
       return 1
     return ((entry.date >= self.date) << 1) - 1
@@ -181,6 +187,7 @@ class Backup:
     self.num = num
     self.rootDir = rootDir
     self.dirSet = set()
+    self.md5sums = dict()
     for src in srcDirs:
       if "" != src:
         through_dirs(src, lambda x: None, repoVerify)
@@ -203,18 +210,18 @@ class Backup:
         command = self.command % (os.path.normpath(path), src)
         self.removePair(path)
         system(command)
-        md5sumCreate(path)
+        self.md5sums[path] = md5sum(path)
         self.removeKey(key, self.destDirs[1:])
       except Exception, e:
-        print "backup error:", e        
+        print "backup error:", e
   # Восстанавливает повреждённые или отсутствующие файлы из зеркальных копий 
   def recoveryDirs(self, key):
     if key in self.dirSet:
       return
     #print "recovery dir %1s" % key
     self.dirSet.add(key)
+    list, other = [], []
     fileDict = dict()
-    list,other=[],[]
     for dst in self.destDirs:
       path = dst + key
       if not os.path.isdir(path):
@@ -232,30 +239,53 @@ class Backup:
               other.append(entry)
             else:
               list.append(entry)
-              entry.date = matcher.group(0) 
+              entry.date = matcher.group(0)
     if len(fileDict) == 0:
       return
+    md5dirs = set()
     for (name, entry) in fileDict.items():
       for dst in self.destDirs:
         path = dst + key + '/' + name
-        if not self.correct(name, path):
-          entry.list.append(dst) 
+        if path in self.md5sums:
+          md5 = self.md5sums[path]
+          md5dirs.add(dst)
+        else:
+          md5 = self.correct(path)
+        if md5 == None:
+          md5dirs.add(dst)
+          entry.list.append(dst)
         else:
           if entry.dir == None:
             entry.dir = dst
+          entry.md5[dst] = md5
     list.sort()
     recovery = other + list[:self.num]
-    remove   = list[self.num:]
+    remove = list[self.num:]
     #print " all=%1s\n  recovery=%2s\n  remove=%3s" % (other + list, recovery, remove)
+    md5files = []
     for f in recovery:
       k = key + '/' + f.name
       if f.dir == None:
         print "corrupt error: %1s" % k
-      else: 
+      else:
+        md5files.append(f)
         for dst in f.list:
-          self.copy(f.dir+k, dst+k)
+          f.md5[dst] = f.md5[f.dir]
+          self.copy(f.dir + k, dst + k)
+          self.removeFile(dst + k + ".md5") # устаревший файл
     for f in remove:
       self.removeKey(key + '/' + f.name, self.destDirs)
+    for dst in md5dirs:
+      fd = None
+      try:
+        fd = open(dst + key + "/.md5", "wb")
+        for f in md5files:
+          fd.write("%s\t*%s\n" % (f.md5[dst], f.name))
+      except Exception, e:
+        print "md5sum error:", e
+      finally:
+        close(fd)
+
   # Удаляет файл в заданных директориях
   def removeKey(self, key, destDirs):
     for dir in destDirs:
@@ -267,49 +297,60 @@ class Backup:
       sw = StopWatch("rm %1s" % path)
       self.removeFile(path)
       sw.stop()
-    self.removeFile(self.md5(path))
+    self.removeFile(path + ".md5") # устаревший файл
   # Удаляет файл
   def removeFile(self, path):
     if os.path.isfile(path):
       os.remove(path)
-  # Копирует файл с контрольной суммой
+  # Копирует файл
   def copy(self, src, dst):
     sw = StopWatch("cp %1s %2s" % (src, dst))
     try:
       mkdirs(os.path.dirname(dst))
       shutil.copy(src, dst)
-      shutil.copy(self.md5(src), self.md5(dst))
     except Exception, e:
       print "copy error:", e
     finally:
       sw.stop()
-  # Проверяет контрольую сумму файла
-  def correct(self, name, path):
+  # Проверяет контрольую сумму файла. Возвращает её, или None, если сумма не верна
+  def correct(self, path):
+    fd = None
     try:
-      md5 = self.md5(path)
-      if not os.path.exists(path) or not os.path.exists(md5):
-        return False
-      #print self.dir, ',', k, " line =", readline(md5)
-      m = self.md5Pattern.match(readline(md5))
-      if m == None:
-        return False
-      real = md5sum(path).hexdigest()
-      #print self.dir, ',', k, " real =", real
-      return real == m.group(1) and name == m.group(2)
+      if not os.path.isfile(path):
+        return None
+      dir = os.path.dirname(path) + '/'
+      md5 = dir + ".md5" # контрольные суммы всех файлов директории
+      if not os.path.isfile(md5):
+        md5 = dir + os.path.basename(path) + ".md5" # устаревший файл
+        if not os.path.isfile(md5):
+          return None
+      fd = open(md5, "r")
+      while True:
+        line = fd.readline()
+        if len(line) == 0:
+          return None
+        m = self.md5Pattern.match(line)
+        if m != None and m.group(2) == os.path.basename(path):
+          stored = m.group(1).lower()
+          real = md5sum(path)
+          if stored == real:
+            return real
+          return None
     except Exception, e:
       print "md5sum check error:", e
-      return False
-  # Возвращает имя файла с контрольной суммой
-  def md5(self, path):
-    return path + ".md5"
-              
+      return None
+    finally:
+      close(fd)
+
 if __name__ == '__main__':
   if len(sys.argv) < 6:
-    print "Usage: backup.py destDirs srcDirs archivingCommand fileSuffix numberOfFiles [rootDir]" 
-    print "Example: backup.py /var/backup $HOME/src 'tar czf %1s %2s' tar.gz 3" 
+    print "Usage: backup.py destDirs srcDirs archivingCommand fileSuffix numberOfFiles [rootDir]"
+    print "Example: backup.py /var/backup $HOME/src 'tar czf %1s %2s' tar.gz 3"
   else:
     if len(sys.argv) >= 7:
       root = sys.argv[6]
     else:
       root = socket.gethostname()
+    sw = StopWatch("backup")
     Backup(sys.argv[1].split(","), sys.argv[2].split(","), sys.argv[3], sys.argv[4], int(sys.argv[5]), root)
+    sw.stop()
