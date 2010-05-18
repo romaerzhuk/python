113033582e99722ae9e7df530ebed622c4de96fe
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2010-05-18 04:15:04 +0000

    remove torrents when ratio>=2
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@883 b4698315-7dff-e942-a971-4591eb902e80

--- transmission-switch.py
+++ transmission-switch.py
@@ -3,7 +3,8 @@ import sys, transmissionrpc
 
 def main():
   tc = transmissionrpc.Client('localhost', port=9091)
-  ids = tc.list().keys()
+  torrents = tc.list()
+  ids = torrents.keys()
   if len(sys.argv) > 1:
     command = sys.argv[1]
   else:
@@ -15,6 +16,12 @@ def main():
   else:
     print "usage:"
     print "\ttransmission-switch start|stop"
+  removed=[]
+  for t in torrents.values():
+    if t.ratio >= 2:
+      removed.append(t.id)
+  if len(removed) > 0:
+    tc.remove(removed)
 
 if __name__ == '__main__':
   main()
