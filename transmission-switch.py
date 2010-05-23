2e4185a06ea5af14c54aa83b01d896449123d5c1
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2010-05-23 08:22:17 +0000

    _request => info
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@887 b4698315-7dff-e942-a971-4591eb902e80

--- transmission-switch.py
+++ transmission-switch.py
@@ -6,8 +6,7 @@ def main():
   fields = ['id', 'hashString', 'name', 'sizeWhenDone', 'leftUntilDone'
             , 'eta', 'status', 'rateUpload', 'rateDownload', 'uploadedEver'
             , 'downloadedEver', 'doneDate']
-  torrents = tc._request('torrent-get', {'fields': fields})
-  #torrents = tc.list()
+  torrents = tc.info(arguments=fields)
   ids = torrents.keys()
   if len(sys.argv) > 1:
     command = sys.argv[1]
@@ -24,8 +23,8 @@ def main():
   #print "max_date=", max_date
   removed=[]
   for t in torrents.values():
+    #print t.id, t.hashString, t.date_done, t.progress, t.ratio, t.name
     if t.ratio >= 1.2 or t.progress >= 100 and t.date_done < max_date:
-      #print t.id, t.hashString, t.date_done, t.progress, t.ratio, t.name
       removed.append([t.id, t.hashString])
   #print removed
   if len(removed) > 0:
