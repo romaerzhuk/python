c864d7a069de5acb5dfa0f70431b3da477d192bf
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2010-05-24 17:43:05 +0000

    *
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@892 b4698315-7dff-e942-a971-4591eb902e80

--- transmission-switch.py
+++ transmission-switch.py
@@ -25,7 +25,7 @@ def main():
   for t in torrents.values():
     #print t.id, t.hashString, t.date_done, t.progress, t.ratio, t.name
     if t.ratio >= 1.2 or t.progress >= 100 and t.date_done < max_date:
-      removed.append([t.id, t.hashString])
+      removed.append(t.id)
   #print removed
   if len(removed) > 0:
     tc.remove(removed)
