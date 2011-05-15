93251b1237609e52f5b12f594463ce256b622411
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2011-05-15 08:09:29 +0000

    file .svn/url => svn property aliases
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@1075 b4698315-7dff-e942-a971-4591eb902e80

--- svn.py
+++ svn.py
@@ -7,15 +7,17 @@ def system(command, reader = None, stdin = None, cwd = None):
   """ Запускает процесс.
   Вызывает процедуру для чтения стандартного вывода.
   Возвращает результат процедуры """
+  if reader == None:
+    stdout = sys.stdout
+  else:
+    stdout = subprocess.PIPE
   try:
-    p = subprocess.Popen(command, stdout = subprocess.PIPE, stdin = stdin, cwd = cwd)
+    p = subprocess.Popen(command, stdout = stdout, stdin = stdin, stderr = sys.stderr, cwd = cwd)
   except Exception, e:
     if platform.system() != "Windows":
       raise e
-    p = subprocess.Popen(["cmd.exe", "/c"] + command, stdout = subprocess.PIPE, stdin = stdin, cwd = cwd)
+    p = subprocess.Popen(["cmd.exe", "/c"] + command, stdout = stdout, stdin = stdin, stderr = stderr, cwd = cwd)
   if reader == None:
-    for line in p.stdout:
-      print line.rstrip()
     return p.wait()
   res = reader(p.stdout)
   # дочитывает стандартный вывод, если что-то осталось
@@ -34,11 +36,10 @@ class Main:
     logging.basicConfig(level = logging.INFO, \
                         stream = sys.stdout, \
                         format = "%(message)s")
-    self.props = 'url'
     self.svn = sys.argv[1]
     self.aliases = None
     self.url_val = None
-    self.dir = ''
+    self.root = ''
     list = [self.svn]
     for arg in sys.argv[2:]:
       i = arg.find(':')
@@ -46,34 +47,38 @@ class Main:
       if i > 0:
         val = self.alias(arg[:i])
         if val != None:
-          arg = val + '/' + arg[i + 1:]
+          suffix = arg[i + 1:]
+          if suffix == '':
+            arg = val
+          else:
+            arg = val + '/' + arg[i + 1:]
       list.append(arg)
     log.debug('%s', list)
-    system(list)
+    system(list, stdin = sys.stdin)
   def alias(self, name):
-    """ Возвращает значение из файла .svn/url """
+    """ Возвращает значение из свойства svn pg aliases """
     if self.aliases == None:
-      log.debug("alias(%s)", name)
-      props = self.dir + '.svn/' + self.props
-      while not os.path.isfile(props):
-        self.dir = '../' + self.dir
-        log.debug('%s not found, search from %s', props, dir)
-        if not os.path.isdir(self.dir):
-          log.error('File [.svn/%s] not found', self.props)
+      log.debug('alias(%s)', name)
+      cnt = 0
+      while True:
+        system([self.svn, 'pg', 'aliases', self.root], self.read_alias)
+        if len(self.aliases) > 0:
+          log.debug('svn pg aliases: found. Use root=%s', self.root)
+          break
+        if self.root == '':
+          self.root = '..'
+        else:
+          self.root = '../' + self.root
+        log.debug('svn pg aliases: not found. Search from %s', self.root)
+        if not os.path.isdir(self.root + '/.svn'):
+          self.root = ''
+          log.debug('svn pg aliases: not found. Use root=%s', self.root)
+          break
+        cnt += 1
+        if cnt > 10:
           sys.exit()
-        props = self.dir + '.svn/' + self.props
-      self.aliases = dict()
-      pattern = re.compile(r'^\s*(\S+)\s*=\s*(.+)$')
-      with open(props, "r") as fd:
-        for line in fd:
-          log.debug("line: [%s]", line)
-          m = pattern.match(line)
-          if m == None:
-            log.debug("pattern not matches: [%s]", line)
-          else:
-            self.aliases[m.group(1)] = m.group(2)
     if 'root' == name:
-      return self.dir
+      return self.root
     if 'url' == name:
       return self.url()
     return self.aliases.get(name)
@@ -81,11 +86,21 @@ class Main:
     """ Возвращает URL """
     if self.url_val == None:
       command = [self.svn, 'info']
-      if self.dir != '':
-        command.append(self.dir)
+      if self.root != '':
+        command.append(self.root)
       self.url_val = system(command, self.read_url)
       log.debug("URL: %s", self.url_val)
     return self.url_val
+  def read_alias(self, stdout):
+    """ Читает свойства в виде строк, псевдоним=url, из svn pg url """
+    self.aliases = dict()
+    pattern = re.compile(r'^\s*(\S+)\s*=\s*(.+)$')
+    for line in stdout:
+      m = pattern.match(line)
+      if m == None:
+        log.debug("pattern not matches: [%s]", line)
+      else:
+        self.aliases[m.group(1)] = m.group(2)
   def read_url(self, stdout):
     """ Читает URL из svn info """
     if self.url_val == None:
