350da9c2218bebc3e97d59aa94de1f2f4601ca81
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2011-05-15 11:49:10 +0000

    *
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@1083 b4698315-7dff-e942-a971-4591eb902e80

--- svn.py
+++ svn.py
@@ -3,26 +3,25 @@
 from __future__ import with_statement
 import sys, os, re, platform, logging, subprocess
 
-def system(command, reader = None, stdin = None, cwd = None):
+def system(command, reader = None, stdin = sys.stdin, cwd = None):
   """ Запускает процесс.
   Вызывает процедуру для чтения стандартного вывода.
   Возвращает результат процедуры """
   if reader == None:
-    stdout = sys.stdout
+    stdout, res = sys.stdout, None
   else:
     stdout = subprocess.PIPE
   try:
     p = subprocess.Popen(command, stdout = stdout, stdin = stdin, stderr = sys.stderr, cwd = cwd)
   except Exception, e:
-    if platform.system() != "Windows":
+    if platform.system() != 'Windows':
       raise e
-    p = subprocess.Popen(["cmd.exe", "/c"] + command, stdout = stdout, stdin = stdin, stderr = stderr, cwd = cwd)
-  if reader == None:
-    return p.wait()
-  res = reader(p.stdout)
-  # дочитывает стандартный вывод, если что-то осталось
-  for line in p.stdout:
-    pass
+    p = subprocess.Popen(['cmd.exe', '/c'] + command, stdout = stdout, stdin = stdin, stderr = stderr, cwd = cwd)
+  if reader != None:
+    res = reader(p.stdout)
+    # дочитывает стандартный вывод, если что-то осталось
+    for line in p.stdout:
+      pass
   p.wait()
   return res
 
@@ -31,11 +30,10 @@ class Main:
     """ Выполняет команду svn с заменой trunk:, branch:, tag:  на соответствующие URL-ы
        Предопределены псевдонимы:
          - root: - корневая директория проекта;
-         - url: - URL текущей ветки проекта.
-        """
+         - url: - URL текущей ветки проекта. """
     logging.basicConfig(level = logging.INFO, \
                         stream = sys.stdout, \
-                        format = "%(message)s")
+                        format = '%(message)s')
     self.svn = sys.argv[1]
     self.aliases = None
     self.url_val = None
@@ -70,7 +68,7 @@ class Main:
         if count > 0:
           list.append(self.root)
     log.debug('%s', list)
-    system(list, stdin = sys.stdin)
+    system(list)
   def alias(self, name):
     """ Возвращает значение из свойства svn pg aliases """
     if self.aliases == None:
@@ -101,7 +99,7 @@ class Main:
       if self.root != '':
         command.append(self.root)
       self.url_val = system(command, self.read_url)
-      log.debug("URL: %s", self.url_val)
+      log.debug('URL: %s', self.url_val)
     return self.url_val
   def read_alias(self, stdout):
     """ Читает свойства в виде строк, псевдоним=url, из svn pg aliases """
@@ -110,7 +108,7 @@ class Main:
     for line in stdout:
       m = pattern.match(line)
       if m == None:
-        log.debug("pattern not matches: [%s]", line)
+        log.debug('pattern not matches: [%s]', line)
       else:
         self.aliases[m.group(1)] = m.group(2)
   def read_url(self, stdout):
@@ -121,9 +119,9 @@ class Main:
         m = pattern.match(line)
         if m != None:
           return m.group(1)
-      log.error("URL not found")
+      log.error('URL not found')
       sys.exit()
 
 if __name__ == '__main__':
-  log = logging.getLogger("svn")
+  log = logging.getLogger('svn')
   Main()
