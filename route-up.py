e9ddce9399d9f0eb2ba253dd2fc700fbd2be121d
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2023-02-09 20:35:46 +0000

    fix 10.0.0.0
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@2530 b4698315-7dff-e942-a971-4591eb902e80

--- route-up.py
+++ route-up.py
@@ -44,6 +44,13 @@ def main():
     log.debug('%s', list)
     for s in list:
         system(['route', 'del', '-net', s[0], 'netmask', s[1]])
+    # DNS: 10.230.192.77 10.230.192.78
+    #system(['route', 'add', '-net', '10.230.192.76', 'netmask', '255.255.255.252', 'dev', 'tunsnx'])
+    # mskxdag.region.vtb.ru 10.132.168.204
+    #system(['route', 'add', '-net', '10.132.168.204', 'netmask', '255.255.255.255', 'dev', 'tunsnx'])
+    # vdi.vtb.ru 10.184.194.178
+    #system(['route', 'add', '-net', '10.184.194.178', 'netmask', '255.255.255.255', 'dev', 'tunsnx'])
+    system(['route', 'add', '-net', '10.0.0.0', 'netmask', '255.0.0.0', 'dev', 'tunsnx'])
 
 if __name__ == '__main__':
     main()
