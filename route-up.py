d5e40df075db643a63860a74df8577f322e2c445
roman <roman@b4698315-7dff-e942-a971-4591eb902e80>
2024-10-08 06:41:28 +0000

    *route-up.py
    git-svn-id: svn+ssh://qnap/home/roman/svn/python@2613 b4698315-7dff-e942-a971-4591eb902e80

--- route-up.py
+++ route-up.py
@@ -22,7 +22,7 @@ def system(command, reader=None, stdin=None, cwd=None):
     p.wait()
     return res
 
-def tunfilter(stdout):
+def tunsnx(stdout):
     pattern = re.compile(r'^(\S+)\s+(\S+)\s+(\S+)\s+.*\stunsnx$')
     includes = re.compile(r'^(10.187.)|(10.189.)|(217.)')
     excludes = re.compile(r'^(217.0.0.0)|(217.8.0.0)|(217.32.0.0)|(217.64.0.0)|(217.128.0.0)')
@@ -36,12 +36,9 @@ def tunfilter(stdout):
         log.debug('%s -> match=%s, exclude=%s, include=%s', line, match, exclude, include)
         if match and (exclude or not include):
             list.append((m.group(1), m.group(3)))
-    return list
-
-def main():
-    log.basicConfig(level='INFO', stream=sys.stdout, format="%(levelname)5s %(lineno)3d %(message)s")
-    list = system(['route', '-n'], tunfilter)
     log.debug('%s', list)
+    if not list:
+        return
     for s in list:
         system(['route', 'del', '-net', s[0], 'netmask', s[1]])
     # DNS: 10.230.192.77 10.230.192.78
@@ -51,6 +48,37 @@ def main():
     # vdi.vtb.ru 10.184.194.178
     #system(['route', 'add', '-net', '10.184.194.178', 'netmask', '255.255.255.255', 'dev', 'tunsnx'])
     system(['route', 'add', '-net', '10.0.0.0', 'netmask', '255.0.0.0', 'dev', 'tunsnx'])
+
+def cscotun(stdout):
+    pattern = re.compile(r'^(\S+)\s+(\S+)\s+(\S+)\s+.*\scscotun0$')
+    excludes = re.compile(r'^(172\.)')
+    list = []
+    for line in stdout:
+        line = line.rstrip().decode('utf-8')
+        m = pattern.match(line)
+        match = m is not None
+        exclude = match and excludes.match(m.group(1)) is not None
+        log.debug('%s -> match=%s, exclude=%s', line, match, exclude)
+        if match and exclude:
+            list.append((m.group(1), m.group(3)))
+    log.debug('%s', list)
+    if not list:
+        return
+    for s in list:
+        system(['route', 'del', '-net', s[0], 'netmask', s[1]])
+    system(['route', 'add', '-net', '172.17.0.0', 'netmask', '255.255.0.0', 'dev', 'docker0'])
+    system(['route', 'add', '-net', '172.18.0.0', 'netmask', '255.255.0.0', 'dev', 'br-ca4dc9d07252'])
+    system(['route', 'add', '-net', '172.19.0.0', 'netmask', '255.255.0.0', 'dev', 'br-df708bb8247e'])
+#172.17.0.0      0.0.0.0         255.255.0.0     U     0      0        0 docker0
+#172.18.0.0      0.0.0.0         255.255.0.0     U     0      0        0 cscotun0
+#172.18.0.0      0.0.0.0         255.255.0.0     U     0      0        0 br-ca4dc9d07252
+#172.19.0.0      0.0.0.0         255.255.0.0     U     0      0        0 cscotun0
+#172.19.0.0      0.0.0.0         255.255.0.0     U     0      0        0 br-df708bb8247e
+
+def main():
+    log.basicConfig(level='INFO', stream=sys.stdout, format="%(levelname)5s %(lineno)3d %(message)s")
+    system(['route', '-n'], tunsnx)
+    #system(['route', '-n'], cscotun)
 
 if __name__ == '__main__':
     main()
